be595a3bce5d1d2423687b637bd1fd6baf518ecd
Dokt0r <ssante@ucm.es>
2024-03-06 18:43:13 +0100

    Favico update
    se ha cambiado el favico para que se mas acorde con el logo del equipo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { RutasModule } from './modules/rutas/rutas.module';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-
 export function HttpLoaderFactory(http: HttpClient){
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
