fe522b629ca85ae35ccb6553237bb1f04e399c43
Alejandro Vida <75375492+AlexVRC3@users.noreply.github.com>
2024-03-08 13:54:20 +0100

    Pruebas unitarias navbar
    Hemos avanzado en las pruebas unitarias aunque tenemos algunas dudas
    
    Co-Authored-By: Santig13 <117572417+Santig13@users.noreply.github.com>

--- src/app/shared/components/navbar/navbar.component.spec.ts
+++ src/app/shared/components/navbar/navbar.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { By } from '@angular/platform-browser';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,6 +11,7 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
       declarations: [ NavbarComponent ]
     })
     .compileComponents();
@@ -21,5 +25,24 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.formNavBar).toBeTruthy();
+    expect(component.formNavBar).toBeInstanceOf(FormGroup); 
   });
+
+  it('El formulario debería obtener el control "search"', () => {
+    const searchControl = component.formNavBar.get('search');
+    expect(searchControl).toBeTruthy();
+    expect(searchControl?.value).toBe('');
+  });
+
+  it('Deberia llamarse al metodo submit cuando se hace click', () => {
+    const button = fixture.debugElement.query(By.css('button'));
+   
+    spyOn(component, 'submit'); 
+    button.nativeElement.click();
+    fixture.detectChanges(); 
+
+    expect(component.submit).toHaveBeenCalled();
+  });
+  
 });
