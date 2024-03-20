65fb8163d71ccec7b484d6acccd5ff18b38096c7
Alejandro Vida <75375492+AlexVRC3@users.noreply.github.com>
2024-03-20 12:45:35 +0100

    El mapa de muestra en la card y mas test
    Co-Authored-By: Santig13 <117572417+Santig13@users.noreply.github.com>

--- src/app/test/modules/detalle-rutas/mapa-ruta/mapa-ruta.component.spec.ts
+++ src/app/test/modules/detalle-rutas/mapa-ruta/mapa-ruta.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapaRutaComponent } from '../../../../modules/rutas/detalle-rutas/mapa-ruta/mapa-ruta.component';
+import GoogleMapService from 'src/app/core/services/google/google-map.service';
+import { Ruta } from 'src/app/core/data/ruta.interface';
 
 describe('MapaRutaComponent', () => {
   let component: MapaRutaComponent;
@@ -8,7 +10,8 @@ describe('MapaRutaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MapaRutaComponent ]
+      declarations: [ MapaRutaComponent ],
+      providers: [GoogleMapService]
     })
     .compileComponents();
   });
@@ -22,4 +25,48 @@ describe('MapaRutaComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Map service should initialize map and draw route successfully', () => {
+    // Arrange
+    const googleMapService = TestBed.inject(GoogleMapService);
+    const ruta: Ruta = {
+      id: 1,
+      name: 'Test Route',
+      ubicacion: 'Test Location',
+      origenLatitud: 0,
+      origenLongitud: 0,
+      destinoLatitud: 1,
+      destinoLongitud: 1
+    };
+
+    // Act
+    googleMapService.initMap(ruta);
+
+    // Assert
+    expect(googleMapService.map).toBeDefined();
+    expect(googleMapService.routeCoordinates.length).toBeGreaterThan(0);
+  });
+  
+      // Initializes the component with a valid input 'rutaFromParent' and calls 'initMap' method of 'GoogleMapService'
+  it('should initialize component with valid input and call initMap method', () => {
+        const fixture = TestBed.createComponent(MapaRutaComponent);
+        const component = fixture.componentInstance;
+        component.rutaFromParent = {
+          id: 1,
+          name: 'Test Ruta',
+          ubicacion: 'Test Ubicacion',
+          origenLatitud: 0,
+          origenLongitud: 0,
+          destinoLatitud: 0,
+          destinoLongitud: 0
+        };
+        const unsubscribeSpy = spyOn(component['googleMapService'], 'initMap');
+        component.ngOnInit();
+        fixture.detectChanges();
+  
+        expect(unsubscribeSpy).toHaveBeenCalledWith(component.rutaFromParent);
+  });
+
+    
+
 });
