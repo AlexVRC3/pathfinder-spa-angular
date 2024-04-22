63c654d6bfec5625e5cbd9a329ec8ba85f3ede7c
josevillacres46 <jvillacr@ucm.es>
2024-04-22 21:50:28 +0200

    Firebase Connection SSL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   URL_API: {
-    PROTOCOL: 'http://',
+    PROTOCOL: 'https://',
     PORT: 0,
-    HOST: 'pathfinder2.eu-north-1.elasticbeanstalk.com'
+    HOST: 'pathfinddser.link'
   },
   PATH: {
     CONTEXT: '',
